7ab876e6038819995d3087a1688327b322748886
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-06-26 16:46:09 -0700

    ✨ Auth Screen

---
+++ lib/common/widgets/custom_textfield.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTextfield extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+
+  const CustomTextfield(
+      {Key? key, required this.controller, required this.hintText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black38),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black38),
+        ),
+      ),
+      validator: (val) {
+        return "";
+      },
+    );
+  }
+}
